41e7714c6e7471601ec89cefe1b498b04094369e
end3r-man <enderman@elg.foundation>
2024-07-23 03:02:19 +0530

    📰 Feature Post Up

--- resources/views/livewire/home/index-pg.blade.php
+++ resources/views/livewire/home/index-pg.blade.php
@@ -1,3 +1,18 @@
-<div class="size-full container mx-auto">
-    <div class="size-full"></div>
+<div class="size-full container mx-auto max-sm:px-2">
+    <div class="w-full h-full flex items-start flex-col">
+        <div class="w-full h-1/3 flex items-center gap-x-2">
+            <div class="h-full max-sm:w-full sm:w-1/2 relative">
+                <div class="w-full h-auto absolute top-0 left-0 z-10 bg-[#11698E]/60 backdrop-blur-sm px-3 py-2">
+                    <h3 class="text-[#F8F1F1] font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                </div>
+                <img class="absolute top-0 left-0 size-full object-cover object-center" src="https://wallpapercave.com/wp/wp13066639.png" alt="feature-img">
+            </div>
+            <div class="h-full max-sm:w-full sm:w-1/2 relative">
+                <div class="w-full h-auto absolute top-0 left-0 z-10 bg-[#11698E]/60 backdrop-blur-sm px-3 py-2">
+                    <h3 class="text-[#F8F1F1] font-semibold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A sequi voluptatum cum excepturi libero eaque quasi!</h3>
+                </div>
+                <img class="absolute top-0 left-0 size-full object-cover object-center" src="https://wallpapercave.com/wp/wp14230856.jpg" alt="feature-img">
+            </div>
+        </div>
+    </div>
 </div>
