c08af2047cfe12c1e758d23eeda0b3daaf68ff45
end3r-man <enderman@elg.foundation>
2024-07-23 14:56:46 +0530

    🚉 Side Area Up

--- resources/views/livewire/home/index-pg.blade.php
+++ resources/views/livewire/home/index-pg.blade.php
@@ -27,6 +27,7 @@
                 <h4 class="text-white font-semibold text-lg">Latest Article</h4>
             </div>
 
+            {{-- Blog Area --}}
             @for ($i = 0; $i < 10; $i++)
                 <div class="w-full h-[480px] md:h-72 bg-slate-800 mb-4 flex items-center max-sm:flex-col md:flex-row hover:shadow-[0px_0px_1px_1px_#11698E]">
                     <div class="w-full md:w-2/5 h-1/2 md:h-full">
@@ -36,7 +37,7 @@
                     <div class="w-full md:w-3/5 h-full flex flex-col gap-y-3 px-4">
                         <h3 class="text-2xl md:text-3xl text-[#11698E] max-sm:line-clamp-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsum rerum dicta?</h3>
                         <p class="text-slate-400 text-sm">By <span class="text-[#11698E] font-semibold">Enderman</span>, about 1 hours ago</p>
-                        <p class="text-slate-300 text-lg leading-6 line-clamp-2 md:text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id doloremque unde facilis numquam architecto suscipit delectus!</p>
+                        <p class="text-slate-300 text-lg leading-6 max-sm:line-clamp-2 md:line-clamp-4 md:text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id doloremque unde facilis numquam architecto suscipit delectus!</p>
                         <div class="w-full h-auto flex items-center gap-x-2">
                             <div class="px-1 md:px-4 py-1 bg-[#11698E] text-slate-300 rounded-md whitespace-nowrap">Gaming</div>
                             <div class="px-1 md:px-4 py-1 bg-[#11698E] text-slate-300 rounded-md whitespace-nowrap">Linux</div>
@@ -51,6 +52,22 @@
         <div class="w-1/4 hidden lg:block">
             <div class="w-full h-12 bg-[#11698E] rounded-t-md flex flex-col items-start justify-center px-4">
                 <h4 class="text-white font-semibold text-lg">Popular Article</h4>
+            </div>
+
+            <div class="w-full flex items-center flex-col bg-slate-800">
+                @for ($i = 0; $i < 5; $i++)
+                    <a href="/" class="text-xl line-clamp-3 text-[#11698E] p-4 border-b border-[#061522]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, accusantium.</a>
+                @endfor
+            </div>
+
+            <div class="w-full h-12 bg-[#11698E] rounded-t-md flex flex-col items-start justify-center px-4 mt-3">
+                <h4 class="text-white font-semibold text-lg">Latest Comments</h4>
+            </div>
+
+            <div class="w-full flex items-center flex-col bg-slate-800">
+                @for ($i = 0; $i < 5; $i++)
+                    <a href="/" class="text-xl line-clamp-3 text-[#11698E] p-4 border-b border-[#061522]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, accusantium.</a>
+                @endfor
             </div>
         </div>
     </section>
